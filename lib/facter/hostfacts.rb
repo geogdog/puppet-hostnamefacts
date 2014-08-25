a6451d216ed61848f24f621ecb12dee9974f0e9f
Greg Trahair <greg.trahair@gmail.com>
2014-08-25 14:13:27 +0200

    Made the code less WET to fix #2 and added the logic to fix #1

--- lib/facter/hostfacts.rb
+++ lib/facter/hostfacts.rb
@@ -7,48 +7,60 @@
 
 module Facter::Util::HostFact
   class << self
+  @@hostname = Facter.value(:hostname)
+  @@hostname_split = @@hostname.split('-')
+  def hostname
+    @@hostname
+  end
+  def hostname_split
+    @@hostname_split
+  end
   def environment
-    Facter.value(:hostname).split('-')[0]
+    @@hostname_split[0]
   end
   def node_number_raw
-    Facter.value(:hostname).split('-')[-1]
+    @@hostname_split[-1]
   end
   def node_number
-    Facter.value(:hostname).split('-')[-1].to_i
+    @@hostname_split[-1].to_i
   end
   def role
-    Facter.value(:hostname).split('-')[1...-1].join('-')
+    @@hostname_split[1...-1].join('-')
   end
   end
 end
 
+if Facter::Util::HostFact.hostname_split.length >= 3
 
-# Cut off the first part of the hostname.  This will be the environment
-# (test, acceptance, production, etc)
-Facter.add('hostfact_environment') do
-  setcode do
-    Facter::Util::HostFact.environment
+  # Cut off the first part of the hostname.  This will be the environment
+  # (test, acceptance, production, etc)
+  Facter.add('hostfact_environment') do
+    setcode do
+      Facter::Util::HostFact.environment
+    end
   end
-end
-
-Facter.add('hostfact_node_number_raw') do
-  setcode do
-    Facter::Util::HostFact.node_number_raw
+  
+  if Facter::Util::HostFact.hostname_split[-1].to_i != 0
+    Facter.add('hostfact_node_number_raw') do
+      setcode do
+        Facter::Util::HostFact.node_number_raw
+      end
+    end
+    
+    Facter.add('hostfact_node_number') do
+      setcode do
+        Facter::Util::HostFact.node_number
+      end
+    end
   end
-end
-
-Facter.add('hostfact_node_number') do
-  setcode do
-    Facter::Util::HostFact.node_number
+  
+  Facter.add('hostfact_role') do
+    setcode do
+      Facter::Util::HostFact.role
+    end
   end
-end
 
-Facter.add('hostfact_role') do
-  setcode do
-    Facter::Util::HostFact.role
-  end
 end
-
 #hostname = Facter.value(:hostname)
 
 ## Get the last element of the hostname.  This should be a poitive integer.
--- spec/unit/facter/hostfacts_spec.rb
+++ spec/unit/facter/hostfacts_spec.rb
@@ -7,24 +7,48 @@ describe 'hostfact_environment', :type => :fact do
   before { Facter.clear }
   after { Facter.clear }
 
-  before do
-    Facter.fact(:hostname).stubs(:value).returns("test-role-01")
+  context "Hostname is test-role-01" do
+      before do
+        Facter.fact(:hostname).stubs(:value).returns("test-role-01")
+      end
+
+      it "should return 'test'" do
+        expect(Facter.fact(:hostfact_environment).value).to eq("test")
+      end
+
+      it "should return 'role'" do
+        expect(Facter.fact(:hostfact_role).value).to eq("role")
+      end
+
+      it "should return '01'" do
+        expect(Facter.fact(:hostfact_node_number_raw).value).to eq("01")
+      end
+
+      it "should return integer 1" do
+        expect(Facter.fact(:hostfact_node_number).value).to eq(1)
+      end
   end
 
-  it "should return test" do
-    expect(Facter.fact(:hostfact_environment).value).to eq("test")
-  end
+  context "Hostname is acc-invalid-host" do
+      before do
+        Facter.fact(:hostname).stubs(:value).returns("acc-invalid-host")
+      end
 
-  it "should return role" do
-    expect(Facter.fact(:hostfact_role).value).to eq("role")
-  end
+      it "should return 'acc'" do
+        expect(Facter.fact(:hostfact_environment).value).to eq("acc")
+      end
 
-  it "should return string 01" do
-    expect(Facter.fact(:hostfact_node_number_raw).value).to eq("01")
-  end
+      it "should return 'invalid'" do
+        expect(Facter.fact(:hostfact_role).value).to eq("invalid")
+      end
 
-  it "should return integer 1" do
-    expect(Facter.fact(:hostfact_node_number).value).to eq(1)
+      it "should return nil" do
+        expect(Facter.fact(:hostfact_node_number_raw)).to eq(nil)
+      end
+
+      it "should return nil" do
+        expect(Facter.fact(:hostfact_node_number)).to eq(nil)
+      end
   end
 
 end
